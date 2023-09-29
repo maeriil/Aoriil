908105b5aa41c2bb2139331e206d797d1c69a774
maeriil <jixanna@gmail.com>
2023-09-29 15:40:07 -0400

    formating changes

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,8 @@
 # def hello_world():
 #     return "<p>asdasd!</p>"
 
-#: flask to get data from frontend (assume we have data for now) (last thing we build in backend)
+#: flask to get data from frontend (assume we have data for now)
+# (last thing we build in backend)
 #: OCR or Pytesseract for extracting text from image
 #: Pillow library to edit on the images
 #: googletrans to translate the text from language 1 to English
@@ -15,21 +16,22 @@
 import cv2
 import pytesseract
 from googletrans import Translator
-from manga_ocr import MangaOcr
-from PIL import Image
 
 MOCKIMAGEPATH = "japanese.png"
 mockImage = cv2.imread(MOCKIMAGEPATH)
 pytesseract_config = r"--oem 3 --psm 5 -l jpn_vert"
 
-grayscale = cv2.cvtColor(mockImage, cv2.COLOR_BGR2GRAY)  # convert image to grayscale
+# convert image to grayscale
+grayscale = cv2.cvtColor(mockImage, cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(grayscale, (9, 9), 0)
-# threshold = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+threshold = cv2.threshold(
+    blur, 0, 255,
+    cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
 # Attempt to remove noise from image
-# kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-# opening = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel, iterations=1)
-# invert = 255 - opening
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+opening = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel, iterations=1)
+invert = 255 - opening
 img = cv2.imread(MOCKIMAGEPATH)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -69,7 +71,7 @@ for cnt in contours:
     cv2.imshow("rectangle is ", rect)
 
     # Cropping the text block for giving input to OCR
-    cropped = im2[y : y + h, x : x + w]
+    cropped = im2[y: y + h, x: x + w]
 
     # Open the file in append mode
     file = open("recognized.txt", "a")
@@ -79,17 +81,17 @@ for cnt in contours:
     print(text)
 
 cv2.waitKey()
-# jp_text = pytesseract.image_to_string(invert, config=pytesseract_config)
-# print(jp_text)
+jp_text = pytesseract.image_to_string(invert, config=pytesseract_config)
+print(jp_text)
 
-# cv2.imshow('mockimage', mockImage)
-# cv2.imshow('blur', blur)
-# cv2.imshow('threshold', threshold)
-# cv2.imshow('opening', opening)
-# cv2.imshow('invert', invert)
-# cv2.waitKey()
+cv2.imshow("mockimage", mockImage)
+cv2.imshow("blur", blur)
+cv2.imshow("threshold", threshold)
+cv2.imshow("opening", opening)
+cv2.imshow("invert", invert)
+cv2.waitKey()
 
-# translator = Translator()
-# translatedText = translator.translate(jp_text, dest="en", src="ja")
+translator = Translator()
+translatedText = translator.translate(jp_text, dest="en", src="ja")
 
-# print('[translated]: ', translatedText.text)
+print("[translated]: ", translatedText.text)
