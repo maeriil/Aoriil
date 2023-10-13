bdb3cabaca69ff6052d771f76221f93f058fcdc4
maeriil <jixanna@gmail.com>
2023-10-13 15:20:02 -0400

    google translate docs

--- backend/modules/translators/google_translator.py
+++ backend/modules/translators/google_translator.py
@@ -1,10 +1,18 @@
+"""
+This module, google_translator, contains the Google Translate API functions
+used to translate the texts into english language. All credentials must be 
+exported out from this page for security reasons. 
+
+"""
+from google.cloud import translate_v2 as translate
+
+
 def translate_text(target: str, text: str) -> dict:
     """Translates text into the target language.
 
     Target must be an ISO 639-1 language code.
     See https://g.co/cloud/translate/v2/translate-reference#supported_languages
     """
-    from google.cloud import translate_v2 as translate
 
     translate_client = translate.Client()
 
